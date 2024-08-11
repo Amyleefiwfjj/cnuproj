1148a35fb62b8fb16f6af335af8e065bc53e7987
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2024-08-12 01:35:53 +0900

    schedule

--- Desktop/CNU/front-master/src/App.js
+++ Desktop/CNU/front-master/src/App.js
@@ -4,12 +4,12 @@ import Home from './pages/Home/Home';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
-
 import MyPage from './components/MyPage/MyPage';
-
+/*import Schedule from './components/schedule/schedule';*/
 import './App.css';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -19,6 +19,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/mypage" element={<MyPage />} />
+          {/*<Route path="/schedule" element={<Schedule />} />*/}
         </Routes>
       </div>
     </Router>
